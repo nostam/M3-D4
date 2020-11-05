0fa087e8b9faf6cf18443d1cbaa6d97a9c7ced88
nostam <matsonho@gmail.com>
2020-11-05 16:30:04 +0100

    M3-D4 ex3

--- index.js
+++ index.js
@@ -4,37 +4,52 @@ const handleErrors = function (response) {
   }
   return response;
 };
-const createCards = function (asin, title, img, price) {
-  return `<div class="card" style="width: 200px">
+const createCards = function (asin, title, img, price, index) {
+  return `<div class="card mx-2" style="width: 200px">
 	  <img src="${img}" class="card-img-top" alt="${title}">
-	  <div class="card-body">
+	  <div class="card-body" id="book${index}">
 		<h5 class="card-title">${title}</h5>
 		<p class="card-text">ASIN : ${asin}</p>
-		<a href="#" class="btn btn-primary">$ ${price}</a>
+		<a href="#" class="btn btn-info">$ ${price}</a>
+		<button type="button" class="btn btn-primary">Add to cart</button>
 	  </div>
 	</div>`;
 };
-const addBooks = function (data) {
-  let tmp = document.createElement("div");
-  tmp.classList = "container row no-gutter";
-  data.forEach(
-    (d) => (tmp.innerHTML += createCards(d.asin, d.title, d.img, d.price))
-  );
-  document.body.append(tmp);
-};
-const loadBooks = function () {
-  fetch("https://striveschool-api.herokuapp.com/books")
-    .then(handleErrors)
-    .then((response) => response.json())
-    .then((data) => addBooks(data))
-    .catch((error) => console.log(error));
-};
-loadBooks();
 
-const addToCartBtn = () => {
-  let btn = document.createElement("div");
-  btn.innerHTML = `<button type="button" class="btn btn-primary">Add to cart</button>`;
-  document.querySelector("h1").append(btn);
+window.onload = () => {
+  const addToCart = (data) => {
+    // console.table(data);
+    let cards = document.getElementsByClassName("card");
+    let addToCartBtn = document.getElementsByClassName("btn-primary");
+    let cart = document.querySelector(".cart");
+    for (let i = 0; i < 50; i++) {
+      addToCartBtn[i].addEventListener("click", function () {
+        cards[i].style.background = "grey";
+        cart.classList.remove("d-none");
+        let p = document.createElement("p");
+        p.innerText = data[i].title;
+        cart.appendChild(p);
+      });
+    }
+  };
+  const addBooks = function (data) {
+    let tmp = document.createElement("div");
+    tmp.classList = "container row no-gutter";
+    data.forEach(
+      (d, index) =>
+        (tmp.innerHTML += createCards(d.asin, d.title, d.img, d.price, index))
+    );
+    document.querySelector(".container").appendChild(tmp);
+    let cards = document.getElementsByClassName("card");
+    let addToCartBtn = document.getElementsByClassName("btn-primary");
+    addToCart(data);
+  };
+  const loadBooks = function () {
+    fetch("https://striveschool-api.herokuapp.com/books")
+      .then(handleErrors)
+      .then((response) => response.json())
+      .then((data) => addBooks(data))
+      .catch((error) => console.log(error));
+  };
+  loadBooks();
 };
-addToCartBtn();
-window.onload = function () {};
